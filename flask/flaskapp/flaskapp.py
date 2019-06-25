02cae836ef31c7cff2fa2764e43ec5837a74e2a1
tonglyu <tonglyu9647@gmail.com>
2019-06-24 22:42:36 -0700

    add template

--- flask/flaskapp/flaskapp.py
+++ flask/flaskapp/flaskapp.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
    
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return "Hello, World!"
+def index():
+    user = {'username': 'tong'}
+    return render_template ( 'index.html', title='Homepage', user=user )
+
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
