679686155d393a6c53eb965551bafae668ed8796
tonglyu <tonglyu9647@gmail.com>
2019-06-24 23:14:11 -0700

    practice extends template

--- flask/flaskapp/flaskapp.py
+++ flask/flaskapp/flaskapp.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask import render_template
-   
+
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     user = {'username': 'tong'}
-    return render_template ( 'index.html', title='Homepage', user=user )
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template ( 'index.html', title='Home', user=user, posts=posts )
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
