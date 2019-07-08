9317d003a010b497051110130e201c52f867e4fb
tonglyu <tonglyu9647@gmail.com>
2019-07-07 18:10:29 -0700

    db select

--- batch_processing/database.sh
+++ batch_processing/database.sh
@@ -10,18 +10,18 @@ select * from pg_indexes where tablename='log_geolocation';
 select cik, geoname_id, sum(count) as total
 from company_geo_table
 where cik = '1288776'
-and date between '2016-01-01' and '2016-01-10'
+and date between '2016-08-01' and '2016-08-31'
 group by cik, geoname_id;
 
 select cik,  country_iso_code, sum(count) as total
 from company_geo_table
 where cik = '1288776'
-and date between '2016-01-01' and '2016-01-10'
+and date between '2016-01-01' and '2016-01-31'
 group by cik,  country_iso_code;
 
 select log.cik as cik, log.total as total, coord.country_name as country_name, coord.region_name as region,
 coord.city_name as city, coord.lat as lat, coord.lng as lng
 from (select cik, geoname_id, sum(count) as total from company_geo_table
-where cik = '1288776' and (date between '2016-01-01' and '2016-01-10')
+where cik = '1288776' and (date between '2016-03-01' and '2016-03-31')
 group by (cik, geoname_id) order by total desc limit 20) log
-left join city_coordinates coord on log.geoname_id = coord.geoname_id limit 20;
+inner join city_coordinates coord on log.geoname_id = coord.geoname_id;
